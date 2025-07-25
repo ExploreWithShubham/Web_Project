b52a9b51c4e75cd097ba08f75a814842d55b2562
AmmanRizwan <ammanrizwan00007@gmail.com>
2025-07-25 16:44:58 +0530

    Refactor project structure: remove unused files, add routing with React Router, and implement NavBar, Footer, and Todo components.

---
+++ frontend/src/pages/todo/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Todo = () => {
+    return (
+        <div className='todo-container'>
+            <h1>Todo List</h1>
+        </div>
+    );
+}
+
+export default Todo;
